b7d7a11c6d8b03f74342cf741af10911cff408fe
unknown <adeosun01pluto>
2023-11-11 20:03:59 +0100

    &apos;

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,8 +13,8 @@ const Footer = () => {
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, idx) => (
+              <FooterColumn key={idx} title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
                     <Link href="/" key={link}>
@@ -27,7 +27,7 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
+                {FOOTER_CONTACT_INFO.links.map((link, idx) => (
                   <Link
                     href="/"
                     key={link.label}
